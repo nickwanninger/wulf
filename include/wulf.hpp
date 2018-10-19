71f01340e02fcf627b79538bfccd11412bba2de5
Nick Wanninger <nickwanninger@gmail.com>
2018-10-19 11:45:07 -0500

    basic structure and file reading

---
+++ include/wulf.hpp
@@ -0,0 +1,10 @@
+#ifndef __WULF__
+#define __WULF__
+
+#include <iostream>
+#include <stdio.h>
+
+
+char* read_file_contents(char*);
+
+#endif
--- src/wulf.cpp
+++ src/wulf.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <wulf.hpp>
 
 int main(int argc, char** argv) {
-	std::cout << "Hello, world\n";
+
+	if (argc <= 1) {
+		std::cout << "Usage: wulfc <file>\n";
+		exit(1);
+	}
+
+
+
+	char* filepath = argv[1];
+
+	char* contents = read_file_contents(filepath);
+	std::cout << contents << "\n";
+
+	// free up the contents
+	delete[] contents;
+
+
 	return 0;
 }
